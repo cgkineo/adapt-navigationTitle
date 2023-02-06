47f1e9bbdbb2166acca177c26686fabef5347bb9
Brad Simpson <brad.simpson@kineo.com>
2023-02-06 11:36:05 -0700

    #5 Add aria-level of 1 if not using course title

--- templates/navigationTitle.jsx
+++ templates/navigationTitle.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function NavigationTitle(props) {
   const {
-    title
+    title,
+    _useCourseTitle
   } = props;
 
   return (
 
-    <div className='navigation-title__inner' dangerouslySetInnerHTML={{ __html: title }} />
+    <div
+      className='navigation-title__inner'
+      dangerouslySetInnerHTML={{ __html: title }}
+      aria-level={!_useCourseTitle ? 1 : null}
+    />
 
   );
 }
