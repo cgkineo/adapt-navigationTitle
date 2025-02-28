2bf67f45dd9a2f650e6e0d0f2990701ad4a1e637
Brad Simpson <brad.simpson@kineo.com>
2025-02-28 08:25:56 -0700

    Add v1.0.3 scripts

--- migrations/v1.js
+++ migrations/v1.js
@@ -47,3 +47,31 @@ describe('Navigation Title - v1.0.0 to v1.0.1', async () => {
   });
   updatePlugin('Navigation Title - update to v1.0.1', { name: 'adapt-navigationTitle', version: '1.0.1', framework: '>=5.0.0' });
 });
+
+describe('Navigation Title - v1.0.1 to v1.0.3', async () => {
+  let courseNavTitle, course, contentObjects;
+  whereFromPlugin('Navigation Title - from v1.0.1', { name: 'adapt-navigationTitle', version: '<1.0.3' });
+  mutateContent('Navigation Title - add course _navigationTitle._isHiddenOnMenu', async (content) => {
+    course = getCourse();
+    courseNavTitle = course._navigationTitle;
+    courseNavTitle._isHiddenOnMenu = false;
+    return true;
+  });
+  mutateContent('Navigation Title - add _navigationTitle._isEnabled to contentObjects', async (content) => {
+    contentObjects = content.filter(({ _type }) => _type === 'page');
+    contentObjects.forEach(contentObject => {
+      _.set(contentObject, '_navigationTitle._isEnabled', true);
+    });
+    return true;
+  });
+  checkContent('Navigation Title - check course _navigationTitle._isHiddenOnMenu', async content => {
+    if (courseNavTitle._isHiddenOnMenu !== false) throw new Error('Navigation Title - course _navigationTitle._isHiddenOnMenu invalid');
+    return true;
+  });
+  checkContent('Navigation Title - check for _navigationTitle._isEnabled on contentObjects', async content => {
+    const isValid = contentObjects.every(({ _navigationTitle }) => _navigationTitle._isEnabled !== undefined);
+    if (!isValid) throw new Error('Navigation Title - _navigationTitle._isEnabled on contentObjects invalid');
+    return true;
+  });
+  updatePlugin('Navigation Title - update to v1.0.3', { name: 'adapt-navigationTitle', version: '1.0.3', framework: '>=5.0.0' });
+});
